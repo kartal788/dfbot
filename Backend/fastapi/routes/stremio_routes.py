e26a17408c5c54a0bd11572c1c13787350b3ecd6
kartal788 <146070323+kartal788@users.noreply.github.com>
2025-12-18 19:47:41 +0300

    Update stremio_routes.py

--- Backend/fastapi/routes/stremio_routes.py
+++ Backend/fastapi/routes/stremio_routes.py
@@ -141,8 +141,6 @@ async def manifest():
                 "type": "series",
                 "id": "released",
                 "name": "Yeni Bölüm",
-                "extra": [{"name": "skip"}],
-                "extraSupported": ["skip"]
             },
             {
                 "type": "movie",
